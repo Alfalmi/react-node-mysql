2fdae2c976e28907dfe1e3fc126aba2aec0f4787
Alfalmi <pinooxd@gmail.com>
2021-08-07 19:50:01 -0700

    create

--- server/index.js
+++ server/index.js
@@ -1,10 +1,33 @@
-const express = require('express');
-const app = express()
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const mysql = require("mysql");
+const cors = require("cors");
 
-app.get("/", (req,res) => {
-    res.send('hellow world');
+const db = mysql.createPool({
+  host: "localhost",
+  user: "pino",
+  password: "Nopinoli8",
+  database: "CRUDDatabase",
+  port: 3306,
+  insecureAuth: "true",
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/api/insert", (req, res) => {
+  const movieName = req.body.movieName;
+  const movieReview = req.body.movieReview;
+
+  const sqlInsert =
+    "INSERT INTO movie_reviews (movie_name, movie_reviews) VALUES (?,?)";
+  db.query(sqlInsert, [movieName, movieReview], (err, result) => {
+    console.log(err);
+  });
 });
 
 app.listen(3001, () => {
-    console.log('running on port 3001');
+  console.log("running on port 3001");
 });
